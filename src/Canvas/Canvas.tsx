e5796a859abdca79ee540d065c296084fc885e1d
onichandame <zxinmyth@gmail.com>
2020-05-05 12:08:54 +0000

    staged

--- src/Canvas/Canvas.tsx
+++ src/Canvas/Canvas.tsx
@@ -1,1 +1,17 @@
-export { Canvas } from 'react-three-fiber'
+import React, { FC, ComponentProps } from 'react'
+import { Canvas as R3FC } from 'react-three-fiber'
+
+const Canvas: FC = ({ children }: ComponentProps<FC>) => {
+  return (
+    <R3FC camera={{
+      isPerspectiveCamera: true,
+      fov: 75,
+      near: 0.1,
+      far: 1000
+    }}>
+      {children}
+    </R3FC>
+  )
+}
+
+export default Canvas
