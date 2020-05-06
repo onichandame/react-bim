c30645983ef8faae53876cd4cba551f6966957d4
onichandame <zxinmyth@gmail.com>
2020-05-06 11:13:27 +0000

    staged. usecontrol

--- src/Canvas/Canvas.tsx
+++ src/Canvas/Canvas.tsx
@@ -1,5 +1,4 @@
 import React, { FC, ComponentProps } from 'react'
-import { animated } from '@react-spring/three'
 import { Canvas as R3FC } from 'react-three-fiber'
 
 export const Canvas: FC = ({ children }: ComponentProps<FC>) => {
@@ -11,7 +10,6 @@ export const Canvas: FC = ({ children }: ComponentProps<FC>) => {
       near: 0.1,
       far: 1000
     }}>
-      <animated.div />
       <ambientLight />
       <pointLight position={[-10, 10, 10]}/>
       <pointLight position={[10, 10, 10]}/>
--- src/Canvas/useControl.ts
+++ src/Canvas/useControl.ts
@@ -1,10 +1,23 @@
-import { useCallback, useEffect } from 'react'
+import { useCallback, useEffect, RefObject } from 'react'
 import { useSpring, config } from '@react-spring/core'
 import { useGesture } from 'react-use-gesture'
 
-export const useControl = (bounds: [number, number], props) => {
-  const [{ y }, set] = useSpring(() => ({ y: 0, config: config.slow }))
-  const fn = useCallback(
+type Props={
+  xBounds: [number, number];
+  yBounds: [number, number];
+  zBounds: [number, number];
+  domTarget: RefObject<EventTarget>;
+}
+
+export const useControl = ({ xBounds, yBounds, zBounds, domTarget }: Props) => {
+  const [{ x, y, z }, set] = useSpring<{x: number;y: number;z: number}>(() => ({ x: 0, y: 0, z: 5, config: config.slow }))
+  const zoom = useCallback<Parameters<typeof useGesture>[0]['onWheel']>(({ wheeling, previous, memo = z.getValue() }) => {
+    if (wheeling) {
+      const newZ = memo + dow
+      return newZ
+    }
+  }, [zBounds, z, set])
+  const drag = useCallback(
     ({ xy: [, cy], previous: [, py], memo = y.getValue() }) => {
       const newY = Math.min(Math.max(memo + cy - py, Math.min(...bounds)), Math.max(...bounds))
       set({ y: newY })
@@ -12,7 +25,7 @@ export const useControl = (bounds: [number, number], props) => {
     },
     [bounds, y, set]
   )
-  const bind = useGesture({ onWheel: fn, onDrag: fn }, props)
-  useEffect(() => props && props.domTarget && bind(), [props, bind])
-  return [y, bind]
+  const bind = useGesture({ onWheel: zoom, onDrag: drag }, { domTarget })
+  useEffect(() => domTarget && bind(), [{ domTarget }, bind])
+  return [{ x, y, z }, bind]
 }
